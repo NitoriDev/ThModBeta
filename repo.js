162016e73bfb74008744561477bdb71355b84a30
NitoriDev <55291986+NitoriDev@users.noreply.github.com>
2019-09-13 19:21:01 +0200

    Fixed being stupîd again

--- repo.js
+++ repo.js
@@ -1,9 +1,9 @@
 {
 	"contact": "if u install that u know lol",
 	"id": "NitoriBeta",
-	"patches": [
+	"patches": {
 		"NitoriShottypes": "Shottype Mod [th12] WARNING: Replay incompatible with unmodded"
-		],
+		},
 	"servers": [
 		"https://raw.githubusercontent.com/NitoriDev/ThModBeta/master"
 	],
