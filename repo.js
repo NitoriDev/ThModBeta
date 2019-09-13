34779048a5e2fcb7227766c75ee2da4f28c631cb
NitoriDev <55291986+NitoriDev@users.noreply.github.com>
2019-09-13 19:19:21 +0200

    Fixed being stupid

--- repo.js
+++ repo.js
@@ -1,10 +1,10 @@
 {
 	"contact": "if u install that u know lol",
 	"id": "NitoriBeta",
-	"patches": {
+	"patches": [
 		"NitoriShottypes": "Shottype Mod [th12] WARNING: Replay incompatible with unmodded"
-		},
-	"servers": {
+		],
+	"servers": [
 		"https://raw.githubusercontent.com/NitoriDev/ThModBeta/master"
 	],
 	"title": "[BETA CHANNEL]Some Touhou mods I made"
