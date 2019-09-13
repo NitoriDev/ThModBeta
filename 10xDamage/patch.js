620318896514ca9927aabbd8d321fffe5735b21f
Nitori <unconfigured@null.spigotmc.org>
2019-09-13 21:29:03 +0200

    10xdamage mod added lol

---
+++ 10xDamage/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "10xDamage",
+	"servers": [
+		"https://raw.githubusercontent.com/NitoriDev/ThModBeta/master/10xDamage/"
+	],
+	"title": "MoF MarisaB 3 power glitch but for every (supported) game, every character, every shottypes and every power level"
+}
--- repo.js
+++ repo.js
@@ -2,6 +2,7 @@
 	"contact": "if u install that u know lol",
 	"id": "NitoriBeta",
 	"patches": {
+		"10xDamage": "MoF MarisaB 3 power glitch but for every (supported) game, every character, every shottypes and every power level",
 		"NitoriShottypes": "Shottype Mod [th12] WARNING: Replay incompatible with unmodded"
 	},
 	"servers": [
