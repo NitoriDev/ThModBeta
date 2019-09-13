1dd9a5b3110bae3b75da7d8c5a97bb9f1a2befcf
NitoriDev <55291986+NitoriDev@users.noreply.github.com>
2019-09-13 19:58:33 +0200

    Add files via upload

---
+++ NitoriShottypes/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "NitoriShottypes",
+	"servers": [
+		"https://raw.githubusercontent.com/NitoriDev/ThModBeta/master/NitoriShottypes/"
+	],
+	"title": "Shottype Mod [th12] WARNING: Replay incompatible with unmodded"
+}
