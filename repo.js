d13d9a80adab1a4091701035ba8208863fae1d53
NitoriDev <55291986+NitoriDev@users.noreply.github.com>
2019-09-13 19:14:12 +0200

    Add files via upload

---
+++ repo.js
@@ -0,0 +1,11 @@
+{
+	"contact": "if u install that u know lol",
+	"id": "NitoriBeta",
+	"patches": {
+		"NitoriShottypes": "Shottype Mod [th12] WARNING: Replay incompatible with unmodded"
+		},
+	"servers": {
+		"https://raw.githubusercontent.com/NitoriDev/ThModBeta/master"
+	],
+	"title": "[BETA CHANNEL]Some Touhou mods I made"
+}
